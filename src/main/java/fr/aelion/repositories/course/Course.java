5ca2172c05a38b1f11d2d746f7a5a155261a8e6f
EstebanP <esteban.petre55555@gmail.com>
2023-02-21 09:47:24 +0100

    Course creation

---
+++ src/main/java/fr/aelion/repositories/course/Course.java
@@ -0,0 +1,13 @@
+package fr.aelion.repositories.course;
+
+import fr.aelion.models.course.Media;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+    private Set<Media> medias = new HashSet<>();
+
+    private String title;
+}
